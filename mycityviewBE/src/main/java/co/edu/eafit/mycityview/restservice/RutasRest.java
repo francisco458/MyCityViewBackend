ebcb14e0e26ae73098e6348a99474bf93100c95f
andres.ospina <davidneopunk@gmail.com>
2015-09-09 18:09:49 -0500

    inclusion metodo consultarutabyid

--- mycityviewBE/src/main/java/co/edu/eafit/mycityview/restservice/RutasRest.java
+++ mycityviewBE/src/main/java/co/edu/eafit/mycityview/restservice/RutasRest.java
@@ -1,5 +1,8 @@
 package co.edu.eafit.mycityview.restservice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +15,7 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import co.edu.eafit.mycityview.business.RutaBusiness;
 import co.edu.eafit.mycityview.model.Location;
+import co.edu.eafit.mycityview.model.RutaDTO;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
@@ -59,22 +64,67 @@ public class RutasRest {
 	}
 	
 	
+	@RequestMapping(value = "/{identificador}", method = RequestMethod.GET)
+	@Secured("ROLE_MYCITYVIEW_REST_C")
+	public ResponseEntity<RutaDTO> consultarRutaById(@PathVariable int identificador) {
+		ResponseEntity<RutaDTO> responseEntity = null;
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.setContentType(MediaType.APPLICATION_JSON);
+		try {
+			RutaDTO ruta = getRutaEjemplo();
+			if(ruta != null){
+				responseEntity = new ResponseEntity<RutaDTO>(ruta, responseHeaders, HttpStatus.OK);
+			}else{
+				responseEntity = new ResponseEntity<RutaDTO>(null, responseHeaders, HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			responseEntity = new ResponseEntity<RutaDTO>(null, responseHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return responseEntity;
+	}
+	
+	
+	private RutaDTO getRutaEjemplo() {
+		RutaDTO ruta = new RutaDTO();
+		ruta.setIdRuta(1);
+		ruta.setNombreRuta("Circular sur 303");
+		List<Location> lista = new ArrayList<Location>();
+		
+		Location loc = new Location();
+		loc.setLatitud(6.1569809);
+		loc.setLongitud(-75.6038274);
+		lista.add(loc);
+		
+		loc = new Location();
+		loc.setLatitud(6.160111199999999);
+		loc.setLongitud(-75.6021118);
+		lista.add(loc);
+		
+		loc = new Location();
+		loc.setLatitud(6.1580352);
+		loc.setLongitud(-75.6043071);
+		lista.add(loc);
+		ruta.setListLocation(lista);
+		return ruta;
+	}
+
+
 	private JsonArray getJsonArrayExample(){
 		JsonArray jsonArray = new JsonArray();
 		
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("nombre", "Circular sur 303");
-		jsonObject.addProperty("identificador", 1);
+		jsonObject.addProperty("nombreRuta", "Circular sur 303");
+		jsonObject.addProperty("idRuta", 1);
 		jsonArray.add(jsonObject);
 		
 		jsonObject = new JsonObject();
-		jsonObject.addProperty("nombre", "Coonatra 150");
-		jsonObject.addProperty("identificador", 2);
+		jsonObject.addProperty("nombreRuta", "Coonatra 150");
+		jsonObject.addProperty("idRuta", 2);
 		jsonArray.add(jsonObject);
 		
 		jsonObject = new JsonObject();
-		jsonObject.addProperty("nombre", "Prado-Medellin");
-		jsonObject.addProperty("identificador", 3);
+		jsonObject.addProperty("nombreRuta", "Prado-Medellin");
+		jsonObject.addProperty("idRuta", 3);
 		jsonArray.add(jsonObject);
 		return jsonArray;
 	}
