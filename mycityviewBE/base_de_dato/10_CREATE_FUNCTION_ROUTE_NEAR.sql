02834d7a35cdf255ce0df8ae3de75ec869ef6d4f
francisco.jimenez <pachoj458>
2015-09-10 15:43:53 -0500

    Se agrega function

---
+++ mycityviewBE/base_de_dato/10_CREATE_FUNCTION_ROUTE_NEAR.sql
@@ -0,0 +1,12 @@
+CREATE DEFINER=`app_user`@`localhost` FUNCTION `get_route_near`(latitud_1 FLOAT, longitud_1 FLOAT, latitud_2 FLOAT, longitud_2 FLOAT, idruta LONG) RETURNS mediumtext CHARSET utf8
+BEGIN
+	DECLARE dist DOUBLE;
+	
+	SET dist = ACOS(SIN(PI() * latitud_1/180.0) * SIN(PI() * latitud_2/180.0) + COS(PI() * latitud_1/180.0) * COS(PI() * latitud_2/180.0)* COS(PI() * longitud_2/180.0 - PI() * longitud_1/180.0)) * 6371 * 1000;
+  	IF(dist > 50) THEN
+		RETURN null;
+	ELSE 
+		RETURN idruta;
+    END IF;
+    
+END
